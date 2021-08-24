7b5bc54ae11b87301d4ffde8a7b38f697297ba8c
rvshekhar10 <rvshekhar10@gmail.com>
2021-08-24 23:47:11 +0530

    small changes

--- js/scripts.js
+++ js/scripts.js
@@ -20,8 +20,8 @@ for (let i = 0; i < $hoverables.length; i++) {if (window.CP.shouldStopExecution(
 // Move the cursor
 window.CP.exitedLoop(0);function onMouseMove(e) {
   TweenMax.to($bigBall, .4, {
-    x: e.pageX - 15,
-    y: e.pageY - 15 });
+    x: e.pageX - 10,
+    y: e.pageY - 10 });
 
   TweenMax.to($smallBall, .1, {
     x: e.pageX - 5,
