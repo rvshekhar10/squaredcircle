fee250ee43a43caa3aeb4e3cc38be5735d0adca9
Rohan Bhuri <rohanbhuri@gmail.com>
2021-08-29 15:09:55 +0530

    Test

--- js/scripts.js
+++ js/scripts.js
@@ -25,19 +25,24 @@ window.CP.exitedLoop(0);function onMouseMove(e) {
 
   TweenMax.to($smallBall, .1, {
     x: e.pageX - 5,
-    y: e.pageY - 7 });
+    y: e.pageY - 5 });
 
 }
 
 // Hover an element
 function onMouseHover() {
   TweenMax.to($bigBall, .3, {
-    scale: 4 });
+    scale: 1 });
+    TweenMax.to($smallBall, .3, {
+        scale: 0 });
 
 }
 function onMouseHoverOut() {
   TweenMax.to($bigBall, .3, {
     scale: 1 });
+    
+    TweenMax.to($smallBall, .3, {
+        scale: 1 });
 
 }
 
@@ -91,3 +96,54 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
 
+(function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+  
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
+  
+  var win = $(window);
+  
+  var allMods = $(".module");
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("already-visible"); 
+    } 
+  });
+  
+  win.scroll(function(event) {
+    
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("come-in"); 
+      } 
+    });
+    
+  });
